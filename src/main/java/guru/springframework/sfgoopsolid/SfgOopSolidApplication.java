096ab0a6bc7220a4aec01120d4d5a09786e7bc3a
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-04-02 15:33:54 +0200

    Initialize Spring.

---
+++ src/main/java/guru/springframework/sfgoopsolid/SfgOopSolidApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgoopsolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgOopSolidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgOopSolidApplication.class, args);
+	}
+
+}
